28b45074923eff1a1bbd998545261c6d65399a3b
Dominik Hadl <nick@dynamicdust.com>
2016-01-19 21:31:56 +0100

    some cleanup in main

---
+++ model-generator/Support/ModelGeneratorCLI.swift
@@ -0,0 +1,74 @@
+//
+//  ModelGeneratorCLI.swift
+//  model-generator
+//
+//  Created by Dominik Hádl on 19/01/16.
+//  Copyright © 2016 Nodes. All rights reserved.
+//
+
+import Foundation
+
+public final class ModelGeneratorCLI: CommandLine {
+
+    let sourceCode = StringOption(
+        shortFlag: "s",
+        longFlag: "source-code",
+        required: true,
+        helpMessage: "Source code for model generator to generate model boilerplate code.")
+
+    let moduleName = StringOption(
+        shortFlag: "m",
+        longFlag: "module-name",
+        required: false,
+        helpMessage: "Optional module name to prepend to a struct/class name.")
+
+    let nativeDictionaries = BoolOption(
+        shortFlag: "n",
+        longFlag: "native-swift-dictionaries",
+        required: false,
+        helpMessage: "Optionally use native swift dictionaries instead of NSDictionary in generated model code.")
+
+    let camelCaseConversion = BoolOption(
+        shortFlag: "c",
+        longFlag: "no-convert-camel-case",
+        required: false,
+        helpMessage: "Optionally don't convert property keys from camel case to underscores.")
+
+    public init() {
+        super.init()
+        addOptions(sourceCode, moduleName, nativeDictionaries, camelCaseConversion)
+    }
+
+    public func run() {
+        checkInput()
+        generateCode()
+    }
+
+    // MARK: - Private -
+
+    private func checkInput() {
+        do {
+            try parse()
+        } catch {
+            printUsage(error)
+            exit(EX_USAGE)
+        }
+    }
+
+    private func generateCode() {
+        guard let code = sourceCode.value else {
+            exit(EX_NOINPUT)
+        }
+
+        print(sourceCode.value)
+        print(moduleName.value)
+
+        do {
+            let modelCode = try ModelGenerator.modelCodeFromSourceCode(code)
+            print(modelCode)
+            exit(EX_OK)
+        } catch {
+            print("some error \(error)")
+        }
+    }
+}
--- model-generator/main.swift
+++ model-generator/main.swift
@@ -8,41 +8,5 @@
 
 import Foundation
 
-let cli = CommandLine()
-
-let sourceCode = StringOption(
-    shortFlag: "s",
-    longFlag: "sourceCode",
-    required: true,
-    helpMessage: "Source code for model generator to generate model boilerplate code.")
-
-
-let moduleName = StringOption(
-    shortFlag: "m",
-    longFlag: "moduleName",
-    required: false,
-    helpMessage: "Optional module name to prepend to a struct/class name.")
-
-cli.addOptions(sourceCode, moduleName)
-
-do {
-    try cli.parse()
-} catch {
-    cli.printUsage(error)
-    exit(EX_USAGE)
-}
-
-guard let code = sourceCode.value else {
-    exit(EX_NOINPUT)
-}
-
-print(sourceCode.value)
-print(moduleName.value)
-
-do {
-    let modelCode = try ModelGenerator.modelCodeFromSourceCode(code)
-    print(modelCode)
-    exit(EX_OK)
-} catch {
-    print("some error \(error)")
-}
+ModelGeneratorCLI().run()
+exit(EX_OK)
