26242fb8703c161a9f0d10623a4d6f90e3eb887e
Kasper Welner <kawe@nodes.dk>
2016-03-29 18:12:06 +0200

    Added support for only generating initialiser

--- model-generator/Generators/ModelGenerator.swift
+++ model-generator/Generators/ModelGenerator.swift
@@ -12,7 +12,8 @@ public struct ModelGeneratorSettings {
     public var moduleName: String?
     public var noConvertCamelCase: Bool = false
     public var useNativeDictionaries: Bool = false
-
+    public var onlyCreateInitializer: Bool = false
+    
     public init() {}
 }
 
@@ -25,12 +26,19 @@ public struct ModelGenerator {
         // Parse the model and its properties first
         var model        = try ModelParser.modelFromSourceCode(sourceCode)
         model.properties = try PropertyParser.propertiesFromSourceCode(sourceCode, noConvertCamelCaseKeys: settings.noConvertCamelCase)
-
-        // Genereate encodable and decodable code blocks
+        
+        // Genereate decodable code block
         let decodableCode = DecodableCodeGenerator.decodableCodeWithModel(model, useNativeDictionaries: settings.useNativeDictionaries)
+        
+        if settings.onlyCreateInitializer {
+            return decodableCode
+        }
+        
+        // Genereate encodable code block
         let encodableCode = EncodableCodeGenerator.encodableCodeWithModel(model, useNativeDictionaries: settings.useNativeDictionaries)
-
+        
         // Combine both blocks in an extension block and return it
-        return ExtensionCodeGenerator.extensionCodeWithModel(model, moduleName: settings.moduleName, andContent: decodableCode + encodableCode)
+        return ExtensionCodeGenerator.extensionCodeWithModel(model, moduleName: nil, andContent: decodableCode + encodableCode)
     }
 }
+
